11a84e2631bc0853361ee3f4f4a91085bef518a5
mikl-o <ccorralw@asu.edu>
2022-10-21 18:53:04 -0700

    fixed

--- assn3/index.js
+++ assn3/index.js
@@ -25,53 +25,64 @@ async function login(){
     let ask = chalk.cyanBright("Enter your username to log in: ");
     let user_name = prompt(ask);
     let query = `SELECT user_id FROM users WHERE username='${user_name}'`;
-    await query_promise_single(query).then(()=> {
-        console.log(chalk.cyanBright("Logged in successfully..."));
-        console.log(chalk.cyanBright(`Welcome '${user_name}'`));
-    }).catch(()=>{
-        console.log(chalk.red("Account does not exist"));
-        console.log(div);
-        main();
-    });
+
+    // sqlite await
     let users_id = await query_promise_single(query);
-    let user_id = users_id["user_id"];
-    console.log(users_id["user_id"]);
-    await todo_menu(user_id);
+    // if exists
+    if(users_id != null){
+        let user_id = users_id["user_id"];
+        await todo_menu(user_id);
+        return user_id;
+    }
+    // if not exist go main menu
+    else{
+        console.log(chalk.redBright("User doesn't exist!"));
+        await main();
+    }
 }
 
-
+// create an account
 async function create_account(){
     console.log(div);
     console.log(chalk.cyanBright("Create a new account..."));
     let ask = chalk.cyanBright("Enter a username: ")
     let user_name = prompt(ask);
-    let query = `SELECT (user_id) FROM users WHERE username='${user_name}'`;
-    await query_promise_single(query).then(()=> {
+    let query = `SELECT (user_id) FROM users WHERE users.username='${user_name}'`;
+
+    // sqlite query
+    let check = await query_promise_all(query);
+
+    // check if existing
+    if(check.length != 0)
+    {
         console.log(chalk.red("Username already exists, select a new username."));
-        create_account();
-    }).catch(()=>{
+        await create_account();
+    }
+    // if not existing ,create
+    else{
         let query_insert = ("INSERT into users(username) values(?)");
         console.log(chalk.cyanBright("Creating account..."));
-        add_query_promise(query_insert,[user_name]);
+        await add_query_promise(query_insert,[user_name]);
         console.log(chalk.cyanBright("~~~ Account successfully created! ~~~"));
         console.log(div);
-    });
-    let users_id = await query_promise_single(query);
-    let user_id = users_id["user_id"];
-    console.log(users_id["user_id"]);
-    await todo_menu(user_id);
+        let users_id = await query_promise_single(query);
+        let user_id = users_id["user_id"];
+        await todo_menu(user_id);
+        return user_id;
+    }
 }
 
-
-async function todo_menu(user_id){
+// menu for todo
+async function todo_menu(user_id) {
     // menu
     console.log(div);
-    console.log(chalk.cyanBright("TODO Menu"));
+    let todo = chalk.yellowBright("TODO ")
+    console.log(chalk.cyanBright(`${todo}Menu`));
     console.log(chalk.cyanBright("1. See all tasks"));
     console.log(chalk.cyanBright("2. See incomplete tasks"));
     console.log(chalk.cyanBright("3. See complete tasks"));
     console.log(chalk.cyanBright("4. Add a task"));
-    console.log(chalk.cyanBright("5. Set a task as complete"));
+    console.log(chalk.cyanBright("5. Toggle a task's completion"));
     console.log(chalk.cyanBright("0. Log out and exit"));
 
     // input
@@ -79,48 +90,131 @@ async function todo_menu(user_id){
     let menu_choice = prompt(ask);
     console.log(menu_choice)
     // validate input
-    if(Number(menu_choice) < 0 || Number(menu_choice > 5))
-    {
+    if (Number(menu_choice) < 0 || Number(menu_choice > 5)) {
         let query1 = chalk.cyanBright("Choose a valid menu option: ", menu_choice, " not within valid range.");
         menu_choice = prompt(query1);
     }
 
-    //TODO fix this tomorrow, not returning correctly, after complete last 3 functions and submit.
+    // get all tasks for the user
     async function to_do_get_all(user) {
+        let res_id;
+        let res_description;
+        let res_complete;
         console.log(div);
         console.log(chalk.cyanBright("Task list: "));
-        console.log(`'${user}'`)
-        let query = `SELECT * FROM tasks WHERE user_id=${user}`;
-        let res = await query_promise_single(query).then(() => {
-            console.log("i went into the success")
-                let res_id = res["task_id"];
-                let res_complete;
-                let res_description = res["task_desc"];
-                if (Number(res["is_complete"]) === 1) {
-                    res_complete = "YES";
-                } else {
-                    res_complete = "NO";
-                }
-                console.log(`TASK #${res_id} ${res_description} -- DONE: ${res_complete} `);
-        }).catch(() => {
-            console.log(chalk.red("!!! NO TASKS IN LIST !!!"));
-            console.log(chalk.yellow("Add a task?"));
-        });
+        let query = `SELECT * FROM tasks WHERE tasks.user_id=${user}`;
+        // await all query
+        let res = await query_promise_all(query);
+
+        // if not empty
+        if(res.length != 0){
+            for (let r in res) {
+            res_id = res[r]["task_id"];
+            res_description = res[r]["task_desc"];
+            if (Number(res[r]["is_complete"]) === 1) {
+                res_complete = "YES";
+            } else {
+                res_complete = "NO";
+            }
+            console.log(chalk.magentaBright(`TASK #${res_id} ${res_description} -- DONE: ${res_complete} `));
+            }
+        }
+        // if empty
+        else{
+        console.log(chalk.red("!!! NO TASKS IN LIST !!!"));
+        console.log(chalk.yellow("Add a task?"));
+        }
     }
 
-    async function to_do_add(user){
+    // get incomplete tasks
+    async function to_do_get_incomplete(user) {
+        console.log(div);
+        console.log(chalk.yellowBright("INCOMPLETE tasks:"));
+        let query = `SELECT * FROM tasks WHERE tasks.user_id=${user} AND tasks.is_complete=false`;
+        // await query
+        let res = await query_promise_all(query);
+        // if not empty
+        if(res.length != 0){
+            let res_id;
+            let res_description;
+            let res_complete;
+            for (let r in res){
+                res_id = res[r]["task_id"];
+                res_description = res[r]["task_desc"];
+                if (Number(res[r]["is_complete"]) === 1){
+                    res_complete = "YES";
+                } else{
+                    res_complete = "NO";
+                }
+                console.log(chalk.yellowBright(`TASK #${res_id} ${res_description} -- DONE: ${res_complete} `));
+            }
+        }
+        // if empty
+        else{
+            console.log(chalk.red("!!! NO TASKS IN LIST !!!"));
+            console.log(chalk.yellow("Add a task?"));
+        }
+    }
+
+    // toggle completion of a task (extra credit OC!!!)
+    async function to_do_toggle_complete(user){
+        console.log(div);
+        let res = await to_do_get_all(user);
+        let ask = chalk.greenBright("Enter the ID of the task you wish to mark/unmark: ");
+        let task_ID = prompt(ask);
+        // query to update
+        let query = `UPDATE tasks SET is_complete = ((is_complete | 1) - (is_complete & 1)) WHERE tasks.user_id = ${user} AND tasks.task_id=${task_ID}`
+        // awaiting query
+        let query2 = await add_query_promise(query);
+        console.log(div);
+    }
+
+    // get complete tasks
+    async function to_do_get_complete(user) {
+        console.log(div);
+        console.log(chalk.greenBright("COMPLETE tasks:"));
+        let query = `SELECT * FROM tasks WHERE tasks.user_id=${user} AND tasks.is_complete=true`;
+        // await the query
+        let res = await query_promise_all(query);
+        //if not empty
+        if(res.length!=0){
+            let res_id;
+            let res_description;
+            let res_complete;
+            for (let r in res){
+                res_id = res[r]["task_id"];
+                res_description = res[r]["task_desc"];
+                if (Number(res[r]["is_complete"]) === 1){
+                    res_complete = "YES";
+                }
+                else{
+                    res_complete = "NO";
+                }
+                console.log(chalk.greenBright(`TASK #${res_id} ${res_description} -- DONE: ${res_complete} `));
+            }
+        }
+        // if empty
+        else{
+            console.log(chalk.redBright("There are no complete tasks, maybe complete some?"));
+        }
+    }
+
+
+    // add task for user
+    async function to_do_add(user) {
         console.log(div);
         console.log(chalk.cyanBright("Adding a new task..."));
         let ask = chalk.cyanBright("Enter the description for the task: ")
         let description = prompt(ask);
+        // await insert query
         let query_insert = ("INSERT into tasks(user_id,task_desc,is_complete) values(?,?,?)");
-        await add_query_promise(query_insert,[user,description,false]);
+        await add_query_promise(query_insert, [user, description, false]);
         console.log(chalk.cyanBright("Task added..."));
         console.log(div);
     }
 
     // switch case
-    switch(menu_choice){
+    switch (menu_choice) {
         case '0':
             console.log(chalk.red("Logging out and exiting..."));
             return;
@@ -145,42 +239,37 @@ async function todo_menu(user_id){
             await todo_menu(user_id);
             break;
         case '5':
-            console.log(chalk.cyanBright("Marking task as complete..."));
-            await to_do_complete(user_id);
-            await todo_menu();
+            console.log(chalk.cyanBright("Toggling task completion..."));
+            await to_do_toggle_complete(user_id);
+            await todo_menu(user_id);
             break;
     }
 }
 
-
+// sql query for 1 thing
 function query_promise_single(query){
     return new Promise( (resolve, reject) => {
         db.get(query,  (err,rows) => {
             if(err){
                 reject(err.message);
             }
-            if(rows == undefined || rows.length == 0){
-                reject(rows);
-            }
             resolve(rows);
         })
     })
 }
+// sql query for many
 function query_promise_all(query){
     return new Promise( (resolve, reject) => {
         db.all(query,(err,rows) => {
             if(err){
                 reject(err.message);
             }
-            if(rows==undefined || rows.length == 0) {
-                reject(rows);
-            }
             resolve(rows);
         })
     })
 }
 
-
+// add/update queries
 function add_query_promise(query, values){
     return new Promise((resolve, reject)=>{
         db.run(query,values,(err)=>{
@@ -193,16 +282,17 @@ function add_query_promise(query, values){
     })
 }
 
-
+// main menu
 function start(){
+    let todo = chalk.yellowBright("TODO ")
     console.log(chalk.cyanBright("=================================================="));
-    console.log(chalk.cyanBright("Welcome to CPI310 Assignment 3: TODO List"));
+    console.log(chalk.cyanBright(`Welcome to CPI310 Assignment 3: ${todo} List`));
     console.log(chalk.cyanBright("1. Log in"));
     console.log(chalk.cyanBright("2. Create an account"));
     console.log(chalk.cyanBright("3. Exit"));
 }
 
-
+// main loop
 async function main(){
     start();
     let query = chalk.cyanBright("Enter a valid menu option: ");
@@ -213,18 +303,18 @@ async function main(){
         response = prompt(query1);
     }
 
-    switch(response){
-        case '1':
-            console.log(chalk.cyanBright("login"));
-            await login()
-            break
-        case '2':
-            console.log(chalk.cyanBright("creating new user"))
-            await create_account();
-            break
-        case '3':
-            console.log(chalk.cyanBright("exiting"));
-            return
+        switch(response) {
+            case '1':
+                console.log(chalk.cyanBright("login"));
+                await login();
+                break;
+            case '2':
+                console.log(chalk.cyanBright("creating new user"));
+                await create_account();
+                break;
+            case '3':
+                console.log(chalk.cyanBright("exiting"));
+                return;
+        }
     }
-}
 main();
