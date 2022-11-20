f159d7b13378ac25364b807176129ee04ed813d8
mikl-o <ccorralw@asu.edu>
2022-11-20 16:46:30 -0700

    assn5start

--- assn5/index.js
+++ assn5/index.js
@@ -1,58 +1,186 @@
 import express from 'express';
 import { engine } from 'express-handlebars';
+import {open} from 'sqlite';
+import {v4 as uuidv4} from "uuid";
 import sqlite3 from 'sqlite3';
-import sqlite from 'sqlite';
 import bcrypt from "bcrypt";
+import cookieParser from "cookie-parser";
 
-const cookieParser = require('cookie-parser');
 const SALT_ROUNDS = 10;
-const dbPromise = sqlite.open({
+const dbPromise = open({
     filename: './data/todolist.db',
     driver:sqlite3.Database
 })
 const app = express();
 const port = 6969;
 
+app.use(cookieParser())
+
+const authenticationMiddleware = async (req, res, next) => {
+    if (!req.cookies || !req.cookies.authToken) {
+        return next();
+    }
+    const db = await dbPromise;
+    const authToken = await db.get(
+        "SELECT * FROM authtokens WHERE token = ?",
+        req.cookies.authToken
+    );
+    if (!authToken) {
+        return next();
+    }
+    const user = await db.get(
+        "SELECT user_id, username FROM users WHERE user_id = ?",
+        authToken.user_id
+    );
+    req.user = user;
+    next();
+};
+
+app.use(authenticationMiddleware)
+app.use(express.urlencoded())
+// app.use("/static", express.static("./static"));
+app.engine('handlebars',engine());
+app.set('view engine', 'handlebars');
+app.set("views", "./views");
+
+
+
+// renders home handlebars
+app.get("/", async (req, res)=>{
+    const db = await dbPromise;
+    const tasks = await db.all(`
+    SELECT tasks.task_id, tasks.task_desc, tasks.user_id,
+      IIF(tasks.user_id = ?, 1, 0) isByMyself
+    FROM tasks
+    LEFT JOIN users
+    ON users.user_id = tasks.user_id;`,
+        req.users ? req.users.user_id : null);
+    console.log(tasks);
+    res.render("home", { tasks, users: req.users });
+})
+
+app.get("/login",(req,res)=>{
+    res.render("login",{layout:"main"})
+})
+
+// renders registration page
+app.get("/register",(req,res)=> {
+    if(req.user){
+        return res.redirect("/")
+    }
+    res.render("register", {layout: "main"})
+})
+
+app.post("/login",async(req,res)=>{
+    const db = await dbPromise;
+    const{username,password} = req.body
+    if(!username || !password){
+        return res.render("login",{error:"all fields must be filled out"})
+    }
+    try{
+        const user = await db.get("SELECT * FROM users WHERE username=?",username);
+        if(!user){
+            return res.render("login",{ error: "username or password incorrect" })
+        }
+        const passwordMatches = await bcrypt.compare(password, user.passwordHash);
+        if (!passwordMatches) {
+            return res.render("login", { error: "username or password incorrect" });
+        }
+
+        const token = uuidv4();
+
+        await db.run(
+            "INSERT INTO authtokens (token, user_id) VALUES (?, ?);",
+            token,
+            user.user_id
+        );
+        res.cookie("authToken", token);
+    } catch (e) {
+        console.log(e);
+        return res.render("login", { error: "something went wrong" });
+    }
+
+    res.redirect("/");
+})
+
+app.post('/register', async (req,res)=>{
+    if (req.user) {
+        return res.redirect("/");
+    }
+    const {username,password, passwordx2}=req.body;
+    const db = await dbPromise;
+
+
+    if (!username || !password || !passwordx2) {
+        return res.render("register", { error: "all fields are required" });
+    }
+    if (password !== passwordx2) {
+        return res.render("register", { error: "passwords must match" });
+    }
+
+    try {
+        const passwordHash = await bcrypt.hash(password, SALT_ROUNDS);
+        console.log("passwordHash :", passwordHash);
+        await db.run(
+            "INSERT INTO users (username, passwordHash) VALUES (?, ?)",
+            username,
+            passwordHash
+        );
+
+        const createdUser = await db.get(
+            "SELECT * FROM users WHERE username = ?",
+            username
+        );
+
+        const token = uuidv4();
+
+        await db.run(
+            "INSERT INTO authtokens (token, user_id) VALUES (?, ?);",
+            token,
+            createdUser.user_id
+        );
+        res.cookie("authToken", token);
+    }
+    catch (e) {
+        console.log(e);
+        if (
+            e.message === "SQLITE_CONSTRAINT: UNIQUE constraint failed: User.username"
+        ) {
+            return res.render("register", { error: "username already taken" });
+        }
+        return res.render("register", { error: "something went wrong" });
+    }
+
+    res.redirect("/");
+})
+
+app.post("/add_task",async(req,res)=>{
+    if(!req.user){
+        return res.redirect("/")
+    }
+    if(req.body.taskDescription && req.body.length > 500){
+        return res.render("home",{error: "messages must be less than 500 characters"})
+    }
+    const db = await dbPromise
+    await db.run("INSERT INTO tasks(task_desc,user_id) VALUES (?,?)",req.body.taskDescription,req.user.user_id)
+    res.redirect("/")
+})
+
+// app.get('/user/:username', async (req, res) => {
+//     const db = await dbPromise;
+//     const user = await db.get("SELECT user_id, username FROM users WHERE username=?", req.params.username)
+//     if (!user) {
+//         return res.send('user not found');
+//     }
+//     const messages = await db.all("SELECT tasks.task_id, tasks.task_desc, users.username as user FROM tasks LEFT JOIN users ON user.user_id = tasks.user_id WHERE authorId=?;", user.user_id)
+//     res.render("profile", { username: req.params.username, messages, user: req.user })
+// })
+
 async function setup(){
     const db = await dbPromise;
-    await db.migrate();
+    await db.migrate({force:false});
     app.listen(6969,()=>{
         console.log("listening on http://localhost:6969")
     });
 }
 await setup();
-
-app.use(express.urlencoded())
-app.engine('handlebars',engine());
-app.set('view engine', 'handlebars');
-app.set("views", "./views");
-
-// renders home handlebars
-app.get("/", async (req, res)=>{
-    res.render("home",{layout:false})
-})
-// renders registration page
-app.get("/register",(req,res)=> {
-    res.render("home", {layout: false, register: true})
-})
-
-app.post('/register', async (req,res)=>{
-    const db = await dbPromise;
-    const {username, password, passwordx2} = req.body
-    if (password !== passwordx2) {
-        res.render("register", { error: "Passwords must match" });
-        return;
-    }
-    const passwordHash = await bcrypt.hash(password, SALT_ROUNDS);
-    await db.run(
-        "INSERT INTO users (username, password) VALUES (?, ?)",
-        username,
-        passwordHash
-    );
-    res.redirect("/");
-})
-
-// renders tasks html
-app.get("/tasks",(req,res)=>{
-    res.render('tasks',{layout:false})
-})
